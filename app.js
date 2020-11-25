ab57b2176f4a71bbc39e0ac39d5381aaec4b14da
Wellington Diniz <wellingtondiniz84@gmail.com>
2020-11-25 16:29:34 -0300

    configurando o tratamento de erros

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 const rotaServices = require('./routes/services')
 const rotaOrder = require('./routes/order')
 
+app.use(morgan('dev'))
+
 app.use('/services', rotaServices)
 app.use('/order', rotaOrder)
 
+app.use((req, res, next) => {
+  const erro = new Error('Não encontrado')
+  erro.status = 404
+  next(erro)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  return res.send({
+    erro: {
+      mensagem: error.message
+    }
+  })
+})
 
 module.exports = app
